c503360ff9cef2119d81a997caf555131d200bb0
shirley543 <34461815+shirley543@users.noreply.github.com>
2025-05-30 14:35:58 +1000

    Frontend: cell variant (to update)

--- client/src/components/custom/Heatmap.tsx
+++ client/src/components/custom/Heatmap.tsx
@@ -1,4 +1,6 @@
+import { cn } from '@/lib/utils';
 import { Link } from '@tanstack/react-router'
+import { cva } from 'class-variance-authority';
 
 // TODOs
 // Heatmap of gridcells. Will comprise of:
@@ -60,12 +62,45 @@ function getDaysInYear(year: number) {
 interface CellProps {
   date: Date,
   value: number | boolean,
+  threshold?: number,
   note?: string,
 }
+
+const cellVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        outline:
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
 
 function Cell({
   date,
   value,
+  threshold,
   note
 }: CellProps) {
   // console.log(date);
@@ -73,10 +108,15 @@ function Cell({
   // console.log(note);
 
   return (
-    <div className="w-[20px] h-[20px] bg-amber-200 rounded-sm" onClick={() => {
+    // <div className="w-[20px] h-[20px] bg-amber-200 rounded-sm" onClick={() => {
+    //   console.log(date);
+    // }}>
+    // </div>
+    <div className={cn(cellVariants({ variant, size, className }))} onClick={() => {
       console.log(date);
     }}>
-
     </div>
+
+
   )
 }
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -10,11 +10,6 @@ function App() {
   return (
     <div className="text-center">
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
         <Heatmap />
         <p>
           Edit <code>src/routes/index.tsx</code> and save to reload.
