9bbfb5cba4a9b8ebab47e4f19b378a8c11aff273
shirley543 <34461815+shirley543@users.noreply.github.com>
2025-06-30 17:37:35 +1000

    feat: fe: GoalCard added foundation for two variants (description vs. control-only), split GoalIconText into own component

--- client/src/features/goals/components/GoalCard.tsx
+++ client/src/features/goals/components/GoalCard.tsx
@@ -1,7 +1,19 @@
 import { Button } from "@/components/ui/button";
 import Heatmap from "./Heatmap";
 import { DynamicIcon, type IconName } from 'lucide-react/dynamic';
-import { Pencil, SquarePlus } from "lucide-react";
+import { CalendarDays, CalendarPlus, Pencil, SquarePlus } from "lucide-react";
+import GoalIconText from "./GoalIconText";
+
+enum GoalCardType {
+  Description = 'description',
+  ControlOnly = 'control-only'
+}
+
+/**
+ * Goal Card types: 
+ * - Description type: with description, edit, log today, vs.
+ * - Control-only type: with year select, add entry, log today
+ */
 
 interface GoalCardProps {
   title: string,
@@ -9,30 +21,19 @@ interface GoalCardProps {
   baseColour: string,
   iconName: IconName,
   goalThreshold: number,
+  cardType: GoalCardType
 }
 
 /**
  * Goal card: displays goal header (with icon, title, description + edit and add today's entry buttons)
  * @returns 
  */
-const GoalCard: React.FC<GoalCardProps> = ({ title, description, baseColour, iconName, goalThreshold }) => {
-  return (
-    <div className="goal-card bg-white rounded-xl p-2.5 flex flex-col gap-3 shadow-sm">
+const GoalCard: React.FC<GoalCardProps> = ({ title, description, baseColour, iconName, goalThreshold, cardType=GoalCardType.Description }) => {
+  const descriptionTypeContent = (() => {
+    return <>
       <div className="header-container flex flex-row justify-between">
         {/* Icon and Text */}
-        <div className="icon-text-container flex flex-row gap-2 items-center">
-          <div className="icon-container w-9 h-9 rounded-md flex items-center justify-center text-white opacity-70" style={
-            {
-              backgroundColor: baseColour
-            }
-          }>
-            <DynamicIcon name={iconName} />
-          </div>
-          <div className="header-container flex flex-col gap-0">
-            <h2 className="text-base font-semibold">{title}</h2>
-            <h2 className="text-xs font-regular">{description}</h2>
-          </div>
-        </div>
+        <GoalIconText title={title} description={description} baseColour={baseColour} iconName={iconName} />
         {/* Buttons */}
         <div className="buttons-container flex flex-row gap-1">
           <Button variant="secondary" size="icon">
@@ -43,9 +44,40 @@ const GoalCard: React.FC<GoalCardProps> = ({ title, description, baseColour, ico
           </Button>
         </div>
       </div>
+    </>
+  })();
+
+  const controlOnlyTypeContent = ((() => {
+    return <>
+      <div className="header-container flex flex-row justify-between">
+        {/* Year and Calendar button */}
+        <div className="year-calendar-container flex flex-row gap-1">
+          <h2 className="text-xl font-bold">2025</h2>
+          <Button variant="secondary" size="icon">
+            <CalendarDays className="size-4"/>
+          </Button>
+        </div>
+        {/* Buttons */}
+        <div className="buttons-container flex flex-row gap-1">
+          <Button variant="secondary" size="icon">
+            <CalendarPlus className="size-4"/>
+          </Button>
+          <Button variant="secondary" size="icon">
+            <SquarePlus className="size-4"/>
+          </Button>
+        </div>
+      </div>
+    </>
+  }))();
+
+  return (
+    <div className="goal-card bg-white rounded-xl p-2.5 flex flex-col gap-3 shadow-sm">
+      {/* {descriptionTypeContent} */}
+      {cardType === GoalCardType.ControlOnly ? controlOnlyTypeContent : descriptionTypeContent}
       <Heatmap baseColour={baseColour} threshold={goalThreshold}/>
     </div>
   )
 }
+
 
 export { GoalCard, type GoalCardProps };
---
+++ client/src/features/goals/components/GoalIconText.tsx
@@ -0,0 +1,32 @@
+import { DynamicIcon, type IconName } from "lucide-react/dynamic"
+
+interface GoalIconTextProps {
+  title: string,
+  description: string,
+  baseColour: string,
+  iconName: IconName,
+}
+
+/**
+ * Goal icon text: displays goal header (with icon, title, description)
+ * @returns 
+ */
+const GoalIconText: React.FC<GoalIconTextProps> = ({ title, description, baseColour, iconName }) => {
+  return (
+    <div className="icon-text-container flex flex-row gap-2 items-center">
+      <div className="icon-container w-9 h-9 rounded-md flex items-center justify-center text-white opacity-70" style={
+        {
+          backgroundColor: baseColour
+        }
+      }>
+        <DynamicIcon name={iconName} />
+      </div>
+      <div className="header-container flex flex-col gap-0">
+        <h2 className="text-base font-semibold">{title}</h2>
+        <h2 className="text-xs font-regular">{description}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default GoalIconText;
