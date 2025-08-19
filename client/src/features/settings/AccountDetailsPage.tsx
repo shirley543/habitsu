56e49c9b4e3cc497100fe9f7905bcfa63738a64e
shirley.x <shirleyx38@hotmail.com>
2025-08-19 22:28:34 +1000

    feat: fe: Added early-stage account-details and delete-account pages

---
+++ client/src/features/settings/AccountDetailsPage.tsx
@@ -0,0 +1,126 @@
+import { useAppForm } from '../../hooks/form'
+import { useState } from "react";
+import { TopBarClose } from "@/components/custom/TopBar";
+import { getRouteApi, useCanGoBack, useNavigate, useRouter } from '@tanstack/react-router';
+import { UserPublicityType, type UserResponse, CreateUserSchema, type CreateUserDto } from '@habit-tracker/shared';
+import { useCreateUserMutation, useDeleteUserMutation, useUser, useUpdateUserMutation } from './UserApi';
+import { ErrorDialogCategory, ErrorDialogComponent } from '@/components/custom/ErrorComponents';
+import { Button } from '@/components/ui/button';
+import { DeleteDialog } from '@/components/custom/DialogComponents';
+
+// TODOss:
+// - Fix `value` prop on `input` should not be null. Consider using an empty string to clear the component or `undefined` for uncontrolled components.
+
+interface AccountDetailsFormProps {
+  isCreate: boolean;
+  defaultValues?: UserResponse;
+}
+
+const AccountDetailsForm: React.FC<AccountDetailsFormProps> = ({ isCreate, defaultValues }) => {
+  const navigate = useNavigate();
+  const router = useRouter()
+  const canGoBack = useCanGoBack()
+
+  const initialValues = defaultValues;
+
+  const { mutate: updateUserMutateFn } = useUpdateUserMutation();
+  // const { mutate: deleteUserMutateFn } = useDeleteUserMutation();
+
+  const [displayedError, setDisplayedError] = useState<{ category: ErrorDialogCategory, error: Error }| undefined>(undefined);
+
+  const navigateBack = () => {
+    if (canGoBack) {
+      router.history.back();
+    } else {
+      navigate({ to: "/goals"});
+    }
+  }
+
+  const form = useAppForm({
+    defaultValues: initialValues,
+    validators: {
+      onChange: CreateUserSchema,
+    },
+    onSubmit: ({ value }) => {
+      updateUserMutateFn({ id: defaultValues?.id, update: value }, {
+        onSuccess: navigateBack,
+        onError: (error) => setDisplayedError({
+          error: error,
+          category: ErrorDialogCategory.FormSubmissionFailed
+        }),
+      })
+    },
+  });
+
+  // const handleDelete = () => {
+  //   if (defaultValues?.id) {
+  //     deleteUserMutateFn(defaultValues.id, {
+  //       onSuccess: () => navigate({ to: '/goals' }),
+  //       onError: (error) => setDisplayedError({
+  //         error: error,
+  //         category: ErrorDialogCategory.DeleteFailed
+  //       }),
+  //     })
+  //   }
+  // }
+
+  return (
+    <div className="flex flex-col gap-3">
+      {/* Topbar config */}
+      <TopBarClose title="Account Details" 
+        closeCallback={navigateBack}
+      />
+      {/* Form controls container */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          form.handleSubmit()
+        }}
+        className="space-y-6"
+      >
+        <form.AppField name="username">
+          {(field) => <field.TextField label="Username" />}
+        </form.AppField>
+
+        <form.AppField name="email">
+          {(field) => <field.TextField label="Email" />}
+        </form.AppField>
+
+        <form.AppField name="password">
+          {(field) => <field.TextField label="Current Password" />}
+        </form.AppField>
+
+        <form.AppField name="new_password">
+          {(field) => <field.TextField label="New Password (optional)" />}
+        </form.AppField>
+
+        <form.AppField name="confirm_new_password">
+          {(field) => <field.TextField label="Confirm New Password" />}
+        </form.AppField>
+      </form>
+      {(displayedError) && <ErrorDialogComponent
+        error={displayedError.error}
+        category={displayedError.category}
+        isShow={displayedError !== undefined}
+        onClose={() => { 
+          setDisplayedError(undefined) 
+        }}
+      />}
+    </div>
+  )
+}
+
+export function AccountDetailsPage() {
+  const goalId = 1;
+  const { data, isLoading, error } = useUser(goalId);
+  // TODOsss update this to get current logged in user's details for user name, email, current password, etc.
+
+  return (
+    <>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error.message}</div>}
+      {!isLoading && !error && <AccountDetailsForm isCreate={false} defaultValues={data}/>}
+    </>
+  )
+}
