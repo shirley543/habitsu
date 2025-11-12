f643e239c1ef8ab24e577894b2d5d3d215ac7b76
shirley.x <shirleyx38@hotmail.com>
2025-11-12 21:11:56 +1100

    feat: BE added OptionalJwtAuthGuard to protect profiles endpoint

--- backend/src/profiles/profiles.controller.ts
+++ backend/src/profiles/profiles.controller.ts
@@ -3,16 +3,19 @@ import {
   Get,
   Param,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ProfileEntity } from './profiles.entity';
+import { OptionalJwtAuthGuard } from 'src/auth/optional-jwt-auth.guard';
 
 @Controller('profiles')
 @ApiTags('profiles')
 export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
+  @UseGuards(OptionalJwtAuthGuard)
   @Get(':username')
   @ApiOkResponse({ type: ProfileEntity })
   findByUsername(
