a2964005899b9d3ef71006bc9497bd9e03530c30
shirley.x <shirleyx38@hotmail.com>
2025-12-30 10:48:15 +1100

    Initial commit. fix/#51-#52-#53-package-lock-env. Recreated package-lock, fixed env import error

--- apps/backend/src/env/env.ts
+++ apps/backend/src/env/env.ts
@@ -1,17 +1,17 @@
 import { z } from 'zod';
-import ms from 'ms';
+import * as ms from 'ms';
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(8080),
   JWT_SECRET: z.string(),
   JWT_EXPIRY: z
     .string()
+    .default('3600s')
     .refine(
       (val): val is ms.StringValue =>
         typeof ms(val as ms.StringValue) === 'number',
       { message: 'Invalid JWT_EXPIRY' },
-    )
-    .default('3600s'),
+    ),
   SALT_ROUNDS: z.coerce.number().default(10),
   // TODOs #31: node env pass in as dev, prod, test? local? default to dev?
 });
