aad9a17568271844e5a5150763e33e45418df52c
shirley.x <shirleyx38@hotmail.com>
2025-12-30 13:59:22 +1100

    fix: fixed BE bug introduced by linting fix (user incorrectly returned by OptionalJwtAuthGuard)

--- apps/backend/src/auth/optional-jwt-auth.guard.ts
+++ apps/backend/src/auth/optional-jwt-auth.guard.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
   // Override handleRequest so it never throws an error
-  handleRequest(user) {
+  handleRequest(_err, user, _info, _context) {
     /* eslint-disable-next-line @typescript-eslint/no-unsafe-return */
     return user;
   }
