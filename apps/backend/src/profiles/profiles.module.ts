e5d8b1fdf05d2ba4ed76a7ea58481aeb4954cfe1
shirley.x <shirleyx38@hotmail.com>
2026-01-02 21:25:53 +1100

    fixup: to efactor: moved endpoint goals > user/:username to profiles > :username/goals (add missing provider to profiles module)

--- apps/backend/src/profiles/profiles.module.ts
+++ apps/backend/src/profiles/profiles.module.ts
@@ -4,10 +4,11 @@ import { ProfilesController } from './profiles.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersService } from '../users/users.service';
 import { EnvModule } from '../env/env.module';
+import { GoalsService } from '../goals/goals.service';
 
 @Module({
   controllers: [ProfilesController],
-  providers: [ProfilesService, UsersService],
+  providers: [ProfilesService, UsersService, GoalsService],
   imports: [PrismaModule, EnvModule],
   exports: [ProfilesService],
 })
