a9581bbac9575ab66b6c50bd21092499ca775b2a
shirley.x <shirleyx38@hotmail.com>
2026-03-16 11:38:48 +1100

    feat: added global toast when error occurs at tanstack query root provider

--- apps/client/src/integrations/tanstack-query/root-provider.tsx
+++ apps/client/src/integrations/tanstack-query/root-provider.tsx
@@ -1,6 +1,13 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast(`Something went wrong: ${error.message}`)
+    }
+  }),
+})
 
 export function getContext() {
   return {
